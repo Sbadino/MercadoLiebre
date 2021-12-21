ae9bf1308bfc4c66a957de5417975b5a1333d089
Sbadino <badino.santi@gmail.com>
2021-12-21 18:40:42 -0300

    Primer Commit

---
+++ servidor.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use('/css',express.static(path.resolve(__dirname, './node_modules/bootstrap/dist/css')))
+app.use('/js',express.static(path.resolve(__dirname, './node_modules/bootstrap/dist/js')))
+app.use('/js',express.static(path.resolve(__dirname, './node_modules/jquery/dist')))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+});
+
+app.use(express.static(path.resolve(__dirname, './public')));
+
+app.listen(3050, () =>{
+    console.log("Servidor corriendo");
+});
